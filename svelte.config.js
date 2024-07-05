4a26370dea43a274d1cf09b27389d7446ab614cc
ThousandthStar <thousandthstar@gmail.com>
2024-07-05 17:33:04 -0400

    specified prerender

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,21 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			entries: [
+				'/projects/8bit Duels/',
+				'/projects/8bit Duels/8bd-intro.md',
+				'/projects/8bit Duels/8bd-part1.md',
+				'/projects/8bit Duels/8bd-part2.md',
+				'/projects/8bit Duels/8bd-part3.md',
+				'/projects/8bit Duels/8bd-part4.md',
+				'/projects/8bit Duels/8bd-part5.md',
+				'/projects/8bit Duels/8bd-part6.md',
+				'/projects/8bit Duels/8bd-part7.md',
+				'/projects/8bit Duels/8bd-final.md',
+			]
+		}
 	}
 };
 
